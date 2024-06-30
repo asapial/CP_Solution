223af29cf34ecd013e31b66f22fade13f31a4aad
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2024-07-01 04:17:05 +0600

    laptop

---
+++ A_Alex_and_a_Rhombus.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define   Pi     2*acos(0.0)
+#define   ll     long long
+#define   pb     push_back
+#define   mp     make_pair
+#define   MAX    500006
+#define   MAX1   10000008
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define   lcm(a, b)        ((a)*((b)/__gcd(a,b)))
+void solve(){
+
+int n;
+cin>>n;
+
+cout << 2*n*n-2*n+1;
+}
+
+int main(){
+
+int t;
+t=1;
+while(t--){
+solve();
+}
+return 0;
+}
---
+++ A_Upload_More_RAM.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define   Pi     2*acos(0.0)
+#define   ll     long long
+#define   pb     push_back
+#define   mp     make_pair
+#define   MAX    500006
+#define   MAX1   10000008
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define   lcm(a, b)        ((a)*((b)/__gcd(a,b)))
+void solve(){
+
+int n,k;
+cin>>n>>k;
+
+cout<<(n==1?1:((n-1)*k)+1)<<endl;
+}
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
