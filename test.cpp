5795d06415491dad84445e3d2b1d1a1899384412
Abu Syeed Abdullah <abusyeedabdullah@gmail.com>
2024-08-18 22:03:03 +0600

    chotu

--- test.cpp
+++ test.cpp
@@ -1,58 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define   Pi     2*acos(0.0)
+#define   ll     long long
+#define   pb     push_back
+#define   mp     make_pair
+#define   MAX    500006
+#define   MAX1   10000008
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define   lcm(a, b)        ((a)*((b)/__gcd(a,b)))
+#define debug(x) cout<<#x<<" "<<x<<endl;
+void solve(){
 
-void dfs(vector<vector<int>>& graph, int start) {
-    int n = graph.size();
-    vector<bool> visited(n, false);
-    stack<int> stk;
-    
-    stk.push(start);
-    
-    while (!stk.empty()) {
-        int u = stk.top();
-        stk.pop();
-        
-        if (!visited[u]) {
-            cout << u << " ";
-            visited[u] = true;
-        }
-        
-        for (int v : graph[u]) {
-            if (!visited[v]) {
-                stk.push(v);
-            }
-        }
-    }
+
 }
 
-int main() {
-    int n, e; // Number of nodes and edges
-    cout << "Enter the number of nodes: ";
-    cin >> n;
-    cout << "Enter the number of edges: ";
-    cin >> e;
-    
-    vector<vector<int>> graph(n);
-    
-    cout << "Enter the edges (node pairs):\n";
-    for (int i = 0; i < e; ++i) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v);
-        
-        graph[v].push_back(u);
-    }
-    
-    int startNode;
-    cout << "Enter the starting node for DFS: ";
-    cin >> startNode;
-    
-    cout << "DFS traversal starting from node " << startNode << ": ";
-    dfs(graph, startNode);
-    cout << endl;
-    
-    return 0;
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
 }
