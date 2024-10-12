89e0464e6623cfebf4c29c5d9698f0f0a4124eb8
Abu Syeed Abdullah <abusyeedabdullah@gmail.com>
2024-10-12 13:02:17 +0600

    chotu

---
+++ B_Array_Craft.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define   Pi     2*acos(0.0)
+#define   ll     long long
+#define   pb     push_back
+#define   mp     make_pair
+#define   MAX    500006
+#define   MAX1   10000008
+#define   mem(a,v)   memset(a,v,sizeof(a))
+#define   lcm(a, b)        ((a)*((b)/__gcd(a,b)))
+#define debug(x) cout<<#x<<" "<<x<<endl;
+void solve(){
+
+int n,x,y;
+cin>>n>>x>>y;
+
+}
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+solve();
+}
+return 0;
+}
